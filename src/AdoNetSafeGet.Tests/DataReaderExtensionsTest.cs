5b8a390aa5b49aec210742160708935c2dbb6239
Artur10 <arturstylus@gmail.com>
2019-03-31 19:19:35 +0300

    #3: added tests for DataReaderExtensionsTest.

--- src/AdoNetSafeGet.Tests/DataReaderExtensionsTest.cs
+++ src/AdoNetSafeGet.Tests/DataReaderExtensionsTest.cs
@@ -1,54 +1,199 @@
+using Moq;
 using System;
+using System.Data;
 using Xunit;
 
 namespace AdoNetSafeGet.Tests
 {
-    public class DataReaderExtensionsTest
+    public class DataReaderExtensionsTest : IDisposable
     {
+        IDataReader _mockDataReader;
+        public DataReaderExtensionsTest()
+        {
+           _mockDataReader = CreateMockDataReader().Object;
+        }
+
+        private Mock<IDataReader> CreateMockDataReader()
+        {
+            var mockdataReader = new Mock<IDataReader>();
+            mockdataReader.Setup(m => m.FieldCount).Returns(10);
+            mockdataReader.Setup(m => m.GetOrdinal("Int16Column")).Returns(0);
+            mockdataReader.Setup(m => m.GetInt16(0)).Returns(Int16.MaxValue);
+            mockdataReader.Setup(m => m.GetOrdinal("Int32Column")).Returns(1);
+            mockdataReader.Setup(m => m.GetInt32(1)).Returns(Int32.MaxValue);
+            mockdataReader.Setup(m => m.GetOrdinal("Int64Column")).Returns(2);
+            mockdataReader.Setup(m => m.GetInt64(2)).Returns(Int64.MaxValue);
+            mockdataReader.Setup(m => m.GetOrdinal("DecimalColumn")).Returns(3);
+            mockdataReader.Setup(m => m.GetDecimal(3)).Returns(99.9m);
+            mockdataReader.Setup(m => m.GetOrdinal("CharColumn")).Returns(4);
+            mockdataReader.Setup(m => m.GetChar(4)).Returns('c');
+            mockdataReader.Setup(m => m.GetOrdinal("ByteColumn")).Returns(5);
+            mockdataReader.Setup(m => m.GetByte(5)).Returns(0x00C9);
+            mockdataReader.Setup(m => m.GetOrdinal("StringColumn")).Returns(6);
+            mockdataReader.Setup(m => m.GetString(6)).Returns("s");
+            mockdataReader.Setup(m => m.GetOrdinal("DateTimeColumn")).Returns(7);
+            mockdataReader.Setup(m => m.GetDateTime(7)).Returns(new DateTime(2017, 03, 20));
+            mockdataReader.SetupSequence(m => m.Read())
+                .Returns(true);
+            return mockdataReader;
+        }
+
         [Fact]
         public void SafeGetInt16_DataReaderWithInt16Value_ReturnInt16()
         {
+            //Arrage
+            Int16 expectedValue = Int16.MaxValue;
+            Int16 realResult = default(Int16);
+            var mockDataReader = _mockDataReader;
+            
+            //Act
+            while (mockDataReader.Read())
+            {
+                realResult = mockDataReader.SafeGetInt16("Int16Column");
+            }
+
+            //Assert
+            Assert.Equal(expectedValue, realResult);
+        }
+
+        [Fact]
+        public void SafeGetInt32_DataReaderWithInt32Value_ReturnInt32()
+        {
+            //Arrage
+            Int32 expectedValue = Int32.MaxValue;
+            Int32 realResult = default(Int32);
+            var mockDataReader = _mockDataReader;
+
+            //Act
+            while (mockDataReader.Read())
+            {
+                realResult = mockDataReader.SafeGetInt32("Int32Column");
+            }
+
+            //Assert
+            Assert.Equal(expectedValue, realResult);
+        }
+
+        [Fact]
+        public void SafeGetInt64_DataReaderWithInt64Value_ReturnInt64()
+        {
+            //Arrage
+            Int64 expectedValue = Int64.MaxValue;
+            Int64 realResult = default(Int64);
+            var mockDataReader = _mockDataReader;
+
+            //Act
+            while (mockDataReader.Read())
+            {
+                realResult = mockDataReader.SafeGetInt64("Int64Column");
+            }
+
+            //Assert
+            Assert.Equal(expectedValue, realResult);
+        }
+
+        [Fact]
+        public void SafeGetDecimal_DataReaderWithDecimalValue_ReturnDecimal()
+        {
+            //Arrage
+            Decimal expectedValue = 99.9m;
+            Decimal realResult = default(Decimal);
+            var mockDataReader = _mockDataReader;
+
+            //Act
+            while (mockDataReader.Read())
+            {
+                realResult = mockDataReader.SafeGetDecimal("DecimalColumn");
+            }
+
+            //Assert
+            Assert.Equal(expectedValue, realResult);
+        }
+
+        [Fact]
+        public void SafeGetChar_DataReaderWithCharValue_ReturnChar()
+        {
+            Char expectedResult = 'c';
+            Char realResult = default(Char);
+            var mockDataReader = _mockDataReader;
+
+            while (mockDataReader.Read())
+            {
+                realResult = mockDataReader.SafeGetChar("CharColumn");
+            }
+
+            Assert.Equal(expectedResult, realResult);
+        }
+
+        [Fact]
+        public void SafeGetByte_DataReaderWithByteValue_ReturnByte()
+        {
+            byte expectedResult = 0x00C9;
+            byte realResult = default(byte);
+
+            var mockDataReader = _mockDataReader;
+
+            while (mockDataReader.Read())
+            {
+                realResult = mockDataReader.SafeGetByte("ByteColumn");
+            }
+
+            Assert.Equal(expectedResult, realResult);
 
         }
-        [Fact]
-        public void SafeGetInt32_DataReaderWithInt32Value_ReturnInt16()
-        {
 
+        [Fact]
+        public void SafeGetString_DataReaderWithStringValue_ReturnString()
+        {
+            string expectedResult = "s";
+            String realResult = default(string);
+
+            var mockDataReader = _mockDataReader;
+
+            while (mockDataReader.Read())
+            {
+                realResult = mockDataReader.SafeGetString("StringColumn");
+            }
+
+            Assert.Equal(expectedResult, realResult);
         }
-        [Fact]
-        public void SafeGetInt64_DataReaderWithInt64Value_ReturnInt16()
-        {
 
+        [Fact]
+        public void SafeGetDateTime_DataReaderWithDateTimeValue_ReturnDateTime()
+        {
+            DateTime expectedResult = new DateTime(2017, 03, 20);
+            DateTime realResult = default(DateTime);
+
+            var mockDataReader = _mockDataReader;
+
+            while (mockDataReader.Read())
+            {
+                realResult = mockDataReader.SafeGetDateTime("DateTimeColumn");
+            }
+
+            Assert.Equal(expectedResult, realResult);
         }
-        [Fact]
-        public void SafeGetDecimal_DataReaderWithDecimalValue_ReturnInt16()
-        {
 
+        [Fact]
+        public void SafeGetDateTimeAsFormatedString_DataReaderWithDateTimeValue_ReturnInt16()
+        {
+            string dateTimeFormat = "yyyyMMddHHmmss";
+            String expectedResult = new DateTime(2017, 03, 20).ToString(dateTimeFormat);
+            String realResult = default(string);
+
+            var mockDataReader = _mockDataReader;
+
+            while (mockDataReader.Read())
+            {
+                realResult = mockDataReader.SafeGetDateTimeAsFormatedString("DateTimeColumn", DateTime.MinValue.ToString(), dateTimeFormat);
+            }
+
+            Assert.Equal(expectedResult, realResult);
         }
-        [Fact]
-        public void SafeGetChar_DataReaderWithInt16Value_ReturnInt16()
-        {
 
-        }
-        [Fact]
-        public void SafeGetByte_DataReaderWithInt16Value_ReturnInt16()
+        public void Dispose()
         {
-
-        }
-        [Fact]
-        public void SafeGetString_DataReaderWithInt16Value_ReturnInt16()
-        {
-
-        }
-        [Fact]
-        public void SafeGetDateTime_DataReaderWithInt16Value_ReturnInt16()
-        {
-
-        }
-        [Fact]
-        public void SafeGetDateTimeAsFormatedString_DataReaderWithInt16Value_ReturnInt16()
-        {
-
+            _mockDataReader = null;
         }
     }
 }
---
+++ src/AdoNetSafeGet.Tests/MockDataReaderConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdoNetSafeGet.Tests
+{
+    internal sealed class MockDataReaderTypedConfig
+    {
+        public string ColumnName { get; set; }
+        public int ColumnIndex { get; set; }
+    }
+}
